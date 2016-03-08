5898a732c214f150ef0f98ff827484c8aa4d04e1
danielkupco <kupcodandroid@gmail.com>
2016-03-08 20:04:08 +0100

    Added commits and contributors listing for specific repository.

--- issuebox/website/views.py
+++ issuebox/website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.views.generic import ListView, DetailView
-import website.models as models
+from .models import *
 
 
 # Create your views here.
@@ -36,22 +36,10 @@ def settings(request,user_id):
     context = RequestContext(request)
     return HttpResponse(template.render(context),user_id)
 
-def repository(request):
-    template = loader.get_template('website/repository.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
-
-def all_repositories(request):
-    template = loader.get_template('website/all_repositories.html')
-    context = RequestContext(request)
-    return HttpResponse(template.render(context))
-
 class RepositoriesView(ListView):
-    model = models.Repository
+    model = Repository
     template_name = 'website/all_repositories.html'
 
 class RepositoryDetails(DetailView):
-    model = models.Repository
+    model = Repository
     template_name = 'website/repository.html'
-

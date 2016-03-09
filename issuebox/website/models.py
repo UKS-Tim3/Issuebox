70a2009ac51c3a4ce169076eee88a06ae31ea6e8
Rade Radišić <radisic.rade@gmail.com>
2016-03-09 19:13:01 +0100

    Some unnecessary fields are now optional. Added default values for font colors. Changed side of ManyToMany relationship between tags and issues to be chosen from issue.

--- issuebox/website/models.py
+++ issuebox/website/models.py
@@ -8,64 +8,27 @@ class Contributor(User):
     def __str__(self):
         return self.username
 
-class Issue(models.Model):
-    PRIORITIES = (
-        ('L', 'Low'),
-        ('M', 'Medium'),
-        ('H', 'High'),        
-    )
-    STATUSES = (
-        (0, 'New'),
-        (1, 'In Progress'),
-        (2, 'Closed'),
-    )
-
-    name = models.CharField(max_length = 50)
-    message = models.CharField(max_length = 300)
-    created = models.DateTimeField('date published')
-    closed = models.DateTimeField('date finished')
-    priority = models.CharField(max_length = 1, choices = PRIORITIES)
-    status = models.CharField(max_length = 1, choices = STATUSES)
-
-    assignee = models.OneToOneField(
-        Contributor,
-        related_name = 'assignee',
-        on_delete = models.CASCADE,
-    )
-    issuer = models.OneToOneField(
-        Contributor,
-        related_name = 'issuer',
-        on_delete = models.CASCADE,
-    )
-
-    def __str__(self):
-        return self.name
-
-class Comment(models.Model):
-    message = models.CharField(max_length = 300)
-    issue = models.ForeignKey(
-        Issue,
-        on_delete = models.CASCADE,
-    )
-    commenter = models.ForeignKey(
-        Contributor,
-        on_delete = models.CASCADE,
-    )
-
-    def __str__(self):
-        return self.message
-
 class Tag(models.Model):
     label = models.CharField(max_length = 50)
-    color = models.CharField(max_length = 20)
-    issues = models.ManyToManyField(Issue)
+    font_color = models.CharField(
+        max_length = 7,
+        default = '#000000',
+    )
+    background_color = models.CharField(
+        max_length = 7,
+        default = '#FFFFFF',
+    )
 
     def __str__(self):
         return self.label
 
 class Repository(models.Model):
     name = models.CharField(max_length = 50)
-    description = models.CharField(max_length = 300)
+    description = models.CharField(
+        max_length = 300,
+        blank = True,
+        null = True,
+    )
     github_url = models.CharField(max_length = 200)
     contributors = models.ManyToManyField(Contributor)
 
@@ -86,6 +49,73 @@ class Commit(models.Model):
     )
     repository = models.ForeignKey(
         Repository,
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.message
+
+class Issue(models.Model):
+    PRIORITIES = (
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),        
+    )
+    STATUSES = (
+        ('0', 'New'),
+        ('1', 'In Progress'),
+        ('2', 'Closed'),
+    )
+
+    name = models.CharField(max_length = 50)
+    message = models.CharField(
+        max_length = 300,
+        blank = True,
+        null = True,
+    )
+    created = models.DateTimeField('date published')
+    closed = models.DateTimeField(
+        'date finished',
+        blank = True,
+        null = True,
+    )
+    priority = models.CharField(max_length = 1, choices = PRIORITIES)
+    status = models.CharField(max_length = 1, choices = STATUSES)
+    tags = models.ManyToManyField(
+        Tag,
+        blank = True,
+    )
+
+    assignee = models.OneToOneField(
+        Contributor,
+        related_name = 'assignee',
+        blank = True,
+        null = True,
+        on_delete = models.CASCADE,
+    )
+    issuer = models.OneToOneField(
+        Contributor,
+        related_name = 'issuer',
+        on_delete = models.CASCADE,
+    )
+    commit = models.OneToOneField(
+        Commit,
+        blank = True,
+        null = True,
+        on_delete = models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Comment(models.Model):
+    message = models.CharField(max_length = 300)
+    issue = models.ForeignKey(
+        Issue,
+        on_delete = models.CASCADE,
+    )
+    commenter = models.ForeignKey(
+        Contributor,
         on_delete = models.CASCADE,
     )
 

53a2c1fcea38c6d90b25b901b6df625e4d1f4a1b
danielkupco <kupcodandroid@gmail.com>
2016-03-13 19:45:49 +0100

    Added django-widget-tweaks module for template modification. Useful for adding Bootstrap classes and attributes to form inputs.

--- issuebox/issuebox/settings.py
+++ issuebox/issuebox/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website',
+    # added for template tweaks
+    'widget_tweaks',
 ]
 
 MIDDLEWARE_CLASSES = [

7257213ed37334c96fba4febb64a062d1b841a9e
Rade Radišić <radisic.rade@gmail.com>
2016-03-23 15:34:21 +0100

    URLs.

--- issuebox/website/urls.py
+++ issuebox/website/urls.py
@@ -4,7 +4,7 @@ from website import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', views.login, name='login'),
-    url(r'^tags/$', views.tags, name='tags'),
+    url(r'^tags/$', views.TagsListView.as_view(), name='tags'),
     url(r'^registration/$', views.registration, name='registration'),
     # website/5/setings ?? treba website/user/5/setings
     url(r'^users/(?P<user_id>\d+)/settings/$', views.settings, name='settings'),
